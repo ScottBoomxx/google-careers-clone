b7a98a51ef12fda89c1c7d604a8384244a11fc30
antidote198 <segundojames@gmail.com>
2022-08-30 19:07:08 +0800

    added styling to MainNav component

--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,18 @@
-const fruits = ["Apple", "Banana", "Orange"];
-const vegetables = ["Cucumber", "Radish"];
+// const fruits = ["Apple", "Banana", "Orange"];
+// const vegetables = ["Cucumber", "Radish"];
 
-console.log([...fruits, ...vegetables]);
+// console.log([...fruits, ...vegetables]);
+
+const developer = {
+  salary: 10000,
+  experience: 4.5,
+  techStack: ["Vue", "HTML", "CSS"],
+  lookingForWork: true,
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  },
+};
+developer.doubleSalary();
+console.log(developer.salary);
+console.log(developer.lookingForWork);
