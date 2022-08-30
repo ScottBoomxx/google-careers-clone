7db73dec50456f2e2d7e7e58423f60bf9337f900
antidote198 <segundojames@gmail.com>
2022-08-30 20:08:46 +0800

    added MainNav test file

--- src/playground.js
+++ src/playground.js
@@ -3,16 +3,23 @@
 
 // console.log([...fruits, ...vegetables]);
 
-const developer = {
-  salary: 10000,
-  experience: 4.5,
-  techStack: ["Vue", "HTML", "CSS"],
-  lookingForWork: true,
-  doubleSalary() {
-    this.salary = this.salary * 2;
-    this.lookingForWork = false;
-  },
-};
-developer.doubleSalary();
-console.log(developer.salary);
-console.log(developer.lookingForWork);
+// const developer = {
+//   salary: 10000,
+//   experience: 4.5,
+//   techStack: ["Vue", "HTML", "CSS"],
+//   lookingForWork: true,
+//   doubleSalary() {
+//     this.salary = this.salary * 2;
+//     this.lookingForWork = false;
+//   },
+// };
+// developer.doubleSalary();
+// console.log(developer.salary);
+// console.log(developer.lookingForWork);
+
+// export const evenOrOdd = (number) => {
+//   if (number % 2 === 0) return "Even";
+//   return "Odd";
+// };
+
+// export const multiply = (num1, num2) => num1 * num2;
