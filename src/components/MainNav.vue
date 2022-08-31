6f49499c43e5c1ef915e7349893e499b29821275
antidote198 <segundojames@gmail.com>
2022-08-31 14:28:47 +0800

    added more styling to MainNav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,26 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-soli border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <a
+          :href="url"
+          class="flex items-center h-full text-[22px] text-[#5f6368]"
+          >{{ company }}</a
+        >
+        <nav class="h-full ml-12">
+          <ul
+            class="flex h-full p-0 m-0 list-none text-[#5f6368] hover:text-[#202124]"
+          >
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0 text-[#5f6368] hover:text-[#202124]"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +35,8 @@ export default {
   data() {
     return {
       company: "Google Careers",
+      url: "http://localhost:8081/",
+      menuItems: ["Teams", "Locations", "Benefits", "Jobs", "Students"],
     };
   },
 };
