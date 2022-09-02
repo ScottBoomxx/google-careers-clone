426f32068aa85be54d2a69daf30ceb4054e727f0
antidote198 <segundojames@gmail.com>
2022-09-02 13:30:01 +0800

    adjusted navbar styling

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-5 py-3 font-medium text-[12px] text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
+    class="px-[18px] py-[9px] font-medium text-[12px] text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
   >
     Sign in
   </button>
