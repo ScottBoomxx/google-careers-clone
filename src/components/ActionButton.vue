176aac7c01fcf0c6a484813218ebc611151724e7
antidote198 <segundojames@gmail.com>
2022-09-05 10:08:35 +0800

    added props and refactored tailwind style commands

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,33 @@
 <template>
-  <button
-    class="px-[18px] py-[9px] font-medium text-[12px] text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-  >
-    Sign in
+  <button :class="buttonClass">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text"],
+  data() {
+    return {
+      primary: true,
+    };
+  },
+  computed: {
+    buttonClass() {
+      return {
+        primary: this.primary,
+      };
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply px-[18px] py-[9px] font-medium text-[12px] rounded;
+}
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -59,7 +59,12 @@
         </nav>
         <div class="flex items-center h-full ml-auto">
           <profile-image v-if="isLoggedIn" data-test="profile-image" />
-          <action-button v-else data-test="login-button" @click="loginUser" />
+          <action-button
+            v-else
+            text="Sign in"
+            data-test="login-button"
+            @click="loginUser"
+          />
         </div>
       </div>
     </div>
@@ -79,7 +84,7 @@ export default {
   data() {
     return {
       company: "Careers",
-      url: "http://localhost:8081/",
+      url: "http://localhost:8080/",
       menuItems: ["Teams", "Locations", "Benefits", "Jobs", "Students"],
       isLoggedIn: false,
     };
